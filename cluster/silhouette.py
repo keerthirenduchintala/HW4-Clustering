84efcc822a071b7bcd0372bf62ff9a09ab43e0ad
Keerthi <keerthi.renduchintala@ucsf.edu>
2026-02-05 21:26:31 -0800

    part of silhoutte

--- cluster/silhouette.py
+++ cluster/silhouette.py
@@ -8,6 +8,7 @@ class Silhouette:
         inputs:
             none
         """
+    pass
 
     def score(self, X: np.ndarray, y: np.ndarray) -> np.ndarray:
         """
@@ -24,3 +25,28 @@ class Silhouette:
             np.ndarray
                 a 1D array with the silhouette scores for each of the observations in `X`
         """
+        # checks
+
+        # X is a numpy array and is a 2D matrix
+        if not isinstance(X, np.ndarray):
+            raise TypeError("X must be a numpy array")
+        
+        if X.ndim != 2:
+            raise ValueError("not a 2D matrix")
+        
+        # Y is a numpy array and is a 1D array
+        if not isinstance(y, np.ndarray):
+            raise TypeError("y must be a numpy array")
+        
+        if y.ndim != 1:
+            raise ValueError("not a 1D matrix")
+        
+        #obs X == obs y
+        if X.shape[0] != y.shape[0]:
+            raise ValueError('different number of observations')
+
+        # Silhouette calculation 
+        ## s(i) = (bi-ai)/(max(bi,ai))
+        ### b(i) = mean distance from point i to points in cluster
+        ### a(i) = mean distance from point i to points in nearest cluster that is not the assigned cluster
+        
